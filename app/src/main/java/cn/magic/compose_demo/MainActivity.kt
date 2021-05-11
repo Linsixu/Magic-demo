69d20efc0073ad6be7e100d6670c144f629d5158
linsixu <linsixu@yy.com>
2021-05-11 11:20:54 +0800

    [1.0.0]更新

--- app/src/main/java/cn/magic/compose_demo/MainActivity.kt
+++ app/src/main/java/cn/magic/compose_demo/MainActivity.kt
@@ -1,67 +1,127 @@
-//package cn.magic.compose_demo
-//
-//import android.os.Bundle
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.compose.foundation.Text
-//import androidx.compose.foundation.background
-//import androidx.compose.foundation.layout.Column
-//import androidx.compose.foundation.layout.Row
-//import androidx.compose.foundation.layout.Spacer
-//import androidx.compose.foundation.layout.padding
-//import androidx.compose.material.MaterialTheme
-//import androidx.compose.material.Scaffold
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.graphics.Color
-//import androidx.compose.ui.platform.setContent
-//import androidx.compose.ui.text.TextStyle
-//import androidx.compose.ui.unit.TextUnit
-//import androidx.compose.ui.unit.dp
-//import androidx.ui.tooling.preview.Preview
-//import cn.magic.compose_demo.ui.ComposedemoTheme
-//import cn.magic.compose_demo.ui.MagicColors
-//import cn.magic.compose_demo.ui.shapes
-//import cn.magic.compose_demo.ui.typography
-//
-//class MainActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContent {
-//            MagicTheme {
-//                Scaffold(
-//                    backgroundColor = Color.White,
-//                    bodyContent = {
-//                        listContent()
-//                    }
-//                )
-//            }
-//        }
-//    }
-//}
-//
-////主要简单用下flutter这些老朋友写写
-//@Composable
-//fun listContent() {
-//    Column(modifier = Modifier.background(Color.DarkGray)) {
-//        Text(text = "测试", style = TextStyle(color = Color.Blue, fontSize = TextUnit.Sp(14)))
-//        Spacer(modifier = Modifier.padding(vertical = 5.dp))
-//        Row {
-//            Text(text = "测试1", style = TextStyle(color = Color.Blue, fontSize = TextUnit.Sp(14)))
-//            Text(text = "测试2", style = TextStyle(color = Color.Blue, fontSize = TextUnit.Sp(14)))
-//            Text(text = "测试3", style = TextStyle(color = Color.Blue, fontSize = TextUnit.Sp(14)))
-//        }
-//    }
-//}
-//
-////Material Design风格
-//@Composable
-//fun MagicTheme(
-//    content: @Composable () -> Unit
-//) {
-//    MaterialTheme(
-//        colors = MagicColors,
-//        typography = typography,
-//        shapes = shapes,
-//        content = content
-//    )
-//}
+package cn.magic.compose_demo
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_float_window.*
+
+
+class MainActivity : AppCompatActivity() {
+    private val JUMP_TO_ANOTHER_ACTIVITY_CODE = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_float_window)
+
+        val permission =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { result: Boolean ->
+                val msg = if (result) "同意" else "拒绝"
+                Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+            }
+
+        request_permission.setOnClickListener {
+            //申请访问通讯录权限
+            permission.launch(Manifest.permission.READ_CONTACTS)
+        }
+
+        val contract = registerJumpToContractCallBack()
+        jumpToContract.setOnClickListener {
+            contract.launch(null)
+        }
+
+        val betweenActivityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+                parserLastActivityData(result)
+            }
+
+        //activity之间intent交互
+        jumpToActivity.setOnClickListener {
+            val intent = Intent(this, TestActivity::class.java)
+            betweenActivityResultLauncher.launch(intent)
+        }
+    }
+
+    private fun parserLastActivityData(result: ActivityResult) {
+        when (result.resultCode) {
+            Activity.RESULT_OK -> {
+                val msg = result.data?.getStringExtra("magic") ?: "没返回intent"
+                Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+            }
+            else -> {
+                Log.i("magic", "返回数据失败")
+            }
+        }
+    }
+
+
+    private fun registerJumpToContractCallBack(): ActivityResultLauncher<Void> {
+        return registerForActivityResult(ActivityResultContracts.PickContact()) { uri: Uri? ->
+            if (uri == null) return@registerForActivityResult
+            Log.i("magic", "uri=$uri")
+            val projection1 = arrayOf(
+                ContactsContract.Contacts._ID,
+                ContactsContract.Contacts.DISPLAY_NAME,
+                ContactsContract.Contacts.HAS_PHONE_NUMBER
+            )
+            val cursor = contentResolver.query(
+                uri,
+                projection1,
+                null,
+                null,
+                null
+            )
+            var contactName: String = ""
+            var phoneNum: String = ""
+            var id = 0L
+            if (cursor != null && cursor.moveToFirst()) {
+                do {
+                    contactName =
+                        cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                    id = cursor.getLong(0)
+                    val phoneProjection = arrayOf(
+                        ContactsContract.CommonDataKinds.Phone.NUMBER
+                    )
+                    //因为一个用户可能有多个电话，所以在数据库上肯定是唯一键值关联电话号码表
+                    val phonesCusor = contentResolver.query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        phoneProjection,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + id,
+                        null,
+                        null
+                    )
+                    if (phonesCusor != null && phonesCusor.moveToFirst()) {
+                        //这里测试我只取数据库中第一行数据
+                        phoneNum = phonesCusor.getString(0)
+//                            do {
+//                                phoneNum = phonesCusor.getString(0)
+//                            } while (phonesCusor.moveToNext())
+                    }
+                    phonesCusor?.close()
+                } while (cursor.moveToNext());
+            }
+            cursor?.close()
+            Toast.makeText(this, "$contactName==$phoneNum", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            JUMP_TO_ANOTHER_ACTIVITY_CODE -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    //成功
+                } else {
+                    //失败
+                }
+            }
+        }
+    }
+}
